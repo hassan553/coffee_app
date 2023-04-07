8f31f270a232fa9aee0aba7e4d8eafd6612e8b46
hassan marzouk <hassanmarzouk042@gmail.com>
2023-04-07 11:24:31 +0200

    login and register repository

---
+++ lib/fetures/auth/data/repository/auth_repo.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+
+class AuthRepo {
+  Future userLogin({email, password}) async {
+    try {
+      Response response = await Dio().post(
+        'https://student.valuxapps.com/api/login',
+        data: {
+          "email": email,
+          "password": password,
+        },
+        queryParameters: {
+          'lang': 'ar',
+          'Content-Type': 'application/json',
+        },
+      );
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+
+  Future userRegister({
+    var name,
+    var phone,
+    var password,
+    var email,
+  }) async {
+    try {
+      Response response = await Dio().post(
+        'https://student.valuxapps.com/api/register',
+        data: {
+          'email': email,
+          'password': password,
+          'phone': phone,
+          'name': name,
+        },
+      );
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+}
